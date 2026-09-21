369f3df5dc6f0a553359c003cd8c42e04ff1eb4a
drowsynicolas <nc64wdt4jz@privaterelay.appleid.com>
2026-09-21 01:32:39 -0400

    Update .lua

--- .lua
+++ .lua
@@ -634,7 +634,7 @@ local function createServerPosMarker(character)
 serverPosMarker.Name = "ServerPosition"
 serverPosMarker.Size = Vector3.new(2, 2, 1)
 serverPosMarker.Color = serverPosColor
-serverPosMarker.Material = Enum.Material.Glass
+serverPosMarker.Material = Enum.Material.Plastic
 serverPosMarker.Transparency = 0.5
 serverPosMarker.Anchored = true
 serverPosMarker.CanCollide = false
